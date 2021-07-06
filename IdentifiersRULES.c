add957785d37593a25a17a4f1f7630eea51af585
GK Sir <hellofcode@hell.you>
2021-07-06 22:05:03 +0530

    Tue06Jul202110:05:03PM+0530

---
+++ IdentifiersRULES.c
@@ -0,0 +1,9 @@
+// int a = 10
+#include<stdio.h>
+void main()
+{
+	int a = 10;
+
+	a = 40;
+	printf("%d \n",a);
+}
---
+++ Special_Seq.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+// #include<conio.h>
+void main()
+{
+
+	int a = 10;
+	printf("123\a4\a56\a7890\n");
+	printf("He\fllo Wor\fld");
+	
+	// getchar();
+
+	printf("123\a45\f678\f90\n");
+	printf("ABCD\bEF\n");
+	printf("colorCod\re");
+}
