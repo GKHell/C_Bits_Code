90ad7a24bc2dc25cde5cf4cb69fea9fe6ccbe77d
GK Sir <hellofcode@hell.you>
2021-07-10 22:16:20 +0530

    Sat10Jul202110:16:20PM+0530

---
+++ LoadTaskCode.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+int main()
+{
+
+	long int data[5] = {10000,40000,60000,35000,56000};
+	int i;
+	// clrscr();
+
+	printf("%ld  \n",data[2] );	
+
+	for(i=0;i<5;i++)
+	{
+		printf("ammount  : %ld\n",data[i]);
+	}
+}
