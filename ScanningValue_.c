7577e11f9c89d6501616f0a77137fed64840a55a
GK Sir <hellofcode@hell.you>
2021-07-10 21:45:00 +0530

    Sat10Jul202109:45:00PM+0530

---
+++ ScanningValue_.c
@@ -0,0 +1,24 @@
+
+/*
+	find the last digit of given number.
+
+	convert odd number into even. (n%2!=0)
+*/
+#include<stdio.h>
+#define A 30
+int main()
+{
+
+	const long int a = 10l;
+
+	// a = 30;
+
+	printf("data : %d\n",a);
+
+	// A = 50;
+	#undef A 
+	#define A 50
+
+	printf("new A : %d\n",A);
+
+}
