2e41d4bb961db92e827314803175b54d1762a445
GK Sir <hellofcode@hell.you>
2021-07-10 22:40:43 +0530

    Sat10Jul202110:40:42PM+0530

--- LoadTaskCode.c
+++ LoadTaskCode.c
@@ -3,14 +3,37 @@
 int main()
 {
 
-	long int data[5] = {10000,40000,60000,35000,56000};
-	int i;
-	// clrscr();
+	 long int data[100];
+	 int n,i,max,id;
 
-	printf("%ld  \n",data[2] );	
+	 printf("Enter no of customers : ");
+	 scanf("%d",&n);
 
-	for(i=0;i<5;i++)
-	{
-		printf("ammount  : %ld\n",data[i]);
-	}
+	 printf("Enter the loan  ammount : \n");
+	 for(i=0;i<n;i++)
+	 {
+
+	 	printf("Enter ammount for customers %d : ",i);
+	 	scanf("%ld",&data[i]);
+
+	 }
+
+	 printf("\n--------------------\n\n");
+	 max = data[0];
+	 id = 0;
+	 for(i=0;i<n;i++)
+	 {
+	 	printf("data [ %d ] = %ld \t",i,data[i]);
+
+	 	if(max < data[i])
+	 	{
+	 		max = data[i];
+	 		id = i;
+	 	}
+	 	getchar();// wait untill a key press ,use getch()
+	 	printf("max : %ld\n",max);
+	 }
+
+	 printf("\n\nMax : %ld \t id : %d\n",max,id);
+
 }
