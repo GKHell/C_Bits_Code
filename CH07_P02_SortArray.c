cf302aa856542539ba8bda285bc9de17c4a143a6
GK Sir <hellofcode@hell.you>
2021-08-03 22:25:00 +0530

    Tue03Aug202110:25:00PM+0530

--- CH07_P02_SortArray.c
+++ CH07_P02_SortArray.c
@@ -1,10 +1,24 @@
 
 #include<stdio.h>
+int a[100];
+int i,n,temp,j;
+
+ void print_array()
+ {
+ 	int k;
+ 	printf(" { | ");
+  for(k=0;k<n;k++)
+	{
+		 
+		printf(" %2d |",a[k]);
+	 
+	}
+ printf(" }\n");
+
+ }
 int main()
 {
-	int a[100];
-	int i,n;
-
+	
 	printf("Enter the size :  ");
 	scanf("%d",&n);
 
@@ -15,6 +29,28 @@ int main()
 		scanf("%d",&a[i]);
 		 
 	}
+	//
+		for(i=0;i<n;i++)
+		{
+
+				print_array(a);
+
+			for(j=i+1;j<n;j++)
+			{
+
+				if(a[i] > a[j])
+				{
+					temp = a[i];
+					a[i] = a[j];
+					a[j] = temp;
+				}
+			}
+			
+				
+
+		}
+
+	//
 	printf("---------------------\n");
 
 	for(i=0;i<n;i++)
