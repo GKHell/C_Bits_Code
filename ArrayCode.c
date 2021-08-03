e7bc0f29187854c9ec44f04a9ae7546fc61146c0
GK Sir <hellofcode@hell.you>
2021-08-03 22:05:56 +0530

    Tue03Aug202110:05:56PM+0530

---
+++ ArrayCode.c
@@ -0,0 +1,31 @@
+
+#include<stdio.h>
+int main()
+{
+
+	int a[100];
+	int i,n,counter=0,sum=1;
+
+	printf("Enter the size :  ");
+	scanf("%d",&n);
+
+	printf("Enter the array : \n");
+	for(i=0;i<n;i++)
+	{
+		printf("a [ %d ] = ",i);
+		scanf("%d",&a[i]);
+		// a[i] = ++counter;
+	}
+
+	printf("---------------------\n");
+
+	for(i=0;i<n;i++)
+	{
+		sum*=a[i];
+		printf("a [ %d ] = %d \t %d\n",i,a[i],sum);
+		
+
+		 
+	}
+	printf("total : %d\n",sum);
+}
